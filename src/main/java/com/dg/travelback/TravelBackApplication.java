a9a087863533b436ac0009e269d52bfd7296953c
daegyu-song <sdk0109@naver.com>
2025-03-23 00:12:37 +0900

    init: 초기 프로젝트

---
+++ src/main/java/com/dg/travelback/TravelBackApplication.java
@@ -0,0 +1,13 @@
+package com.dg.travelback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dg/travelback/TravelBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.travelback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
